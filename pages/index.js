622a9e95fbd8265d107040c1a35bbf540b608606
Akshitha <satmaku@clemson.edu>
2022-11-10 22:24:05 -0500

    JS HTML added

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="chess" className={styles.card}>
+            <h2>Play chess &rarr;</h2>
+            <p>Play chess against computer, the popular board game</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
